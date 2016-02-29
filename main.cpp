19e3832b73b28ab4d7a2d8d6532ca1837640ab2f
Luis96791 <luis.fajardozap@hotmail.com>
2016-02-29 16:25:56 -0600

    Faltan cuatro

--- main.cpp
+++ main.cpp
@@ -9,18 +9,36 @@ using namespace std;
 //devuelve true si todos los elementos de la cola son pares, de lo contrario devuelve false
 bool sonPares(queue<int> mi_cola)
 {
-    return false;
+    while(!mi_cola.empty()){
+        if(mi_cola.front()%2==0){
+            mi_cola.pop();
+        }else
+            return false;
+    }
+    return true;
 }
 
 //devuelve true si todos los elementos de la lista son pares, de lo contrario devuelve false
 bool sonPares(list<int> mi_lista)
 {
-    return false;
+    for(list<int>::iterator list_temp = mi_lista.begin(); list_temp != mi_lista.end(); list_temp++){
+        if(mi_lista.front()%2==0){
+            mi_lista.back();
+        }else
+            return false;
+    }
+    return true;
 }
 
 //devuelve true si str es un elemento de mi_cola, de lo contrario devuelve false
 bool existe(queue<string> mi_cola, string str)
 {
+    while(!mi_cola.empty()){
+        if(mi_cola.front()==str){
+            return true;
+        }
+        mi_cola.pop();
+    }
     return false;
 }
 
